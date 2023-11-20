866bd0a35aa40b355c9749d0be4137a3ef1e7c3a
kimjaeseong <jsk@rightbrain.co.kr>
2023-11-20 22:21:05 +0900

    uuid 설정 반영

--- src/components/page/main/Index.tsx
+++ src/components/page/main/Index.tsx
@@ -1,39 +1,33 @@
-import Module1 from "@components/page/main/Module1"
-import Module2 from "@components/page/main/Module2"
-import { useEffect, useState, useRef } from "react"
+import { useState, useRef } from "react"
 
-
+import { v4 as uuidv4 } from "uuid";
 
 type 프롭스타입 = {
-    아이디: number,
+    아이디: string,
     메세지 : string,
     방향: string,
     활성화 : boolean,
 }
 
-
 function Main() {
     const box = useRef<HTMLInputElement>(null);
-    const [메세지카드, 메세지카드설정] = useState([
+    const [메세지카드, 메세지카드설정] = useState<프롭스타입[]>([
         {
-            아이디: 1,
+            아이디: uuidv4(),
             메세지 : '환경합니다.',
             방향 : '왼쪽',
             활성화 : true,
         },
         {
-            아이디: 2,
+            아이디: uuidv4(),
             메세지 : '환경합니다2.',
             방향 : '오른쪽',
             활성화 : true,
         }
     ]);
     const 더미메세지추가 = () =>{
-       
-
-
         const 새로운메세지 = {
-            아이디 : 메세지카드.length + 1,
+            아이디 : uuidv4(),
             메세지 : Math.random() * 10 + '',
             방향 : '오른쪽',
             활성화: false,
@@ -45,8 +39,7 @@ function Main() {
             }
         );
         setTimeout(() => {
-            if (box.current) box.current.scrollTop = 99999999;
-                
+            if (box.current) box.current.scrollTop = box.current.offsetHeight + 9999;
             메세지카드설정(
                 (이전값)=>{
                     const 업데이트값 = 이전값.map((아이템, 인덱스)=>{
@@ -60,9 +53,6 @@ function Main() {
                 }
             );
         }, 1);
-       
-
-
     }
     
     return (
@@ -75,8 +65,6 @@ function Main() {
                 )
             }
             </div>
-            {/* <Module1 />
-            <Module2 /> */}
         </>
     )
   }
