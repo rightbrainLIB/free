b5fa38dd779605dd9b1a2916ff21c20e7dd2f5fa
kimjaeseong <jsk@rightbrain.co.kr>
2023-11-27 16:04:22 +0900

    setstate props로 전달하게 수정

--- src/components/page/main/Card2.tsx
+++ src/components/page/main/Card2.tsx
@@ -1,13 +1,15 @@
 type 프롭스타입 = {
     메세지? : string,
+    활성화인덱스설정? : React.Dispatch<React.SetStateAction<number>>,
 }
 
 function Card(프롭스:프롭스타입) {
-    
-
+    const 설정 = () =>{
+        프롭스?.활성화인덱스설정?.(4)
+    }
     return (
         <div className={"오른쪽"}>
-            {프롭스.메세지}{" "}<button>수정</button>
+            {프롭스.메세지}{" "}<button onClick={설정}>5번으로 한방에 가게</button>
         </div>
     )
 }
